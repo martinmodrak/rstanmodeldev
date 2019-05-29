f4fbd20f07b8b7b3da44f121cb9f3bf1cc45b24c
martin_cerny <cerny.m@gmail.com>
2019-05-29 17:41:57 +0200

    Prettying the code

--- R/kfold.R
+++ R/kfold.R
@@ -1,60 +1,76 @@
+#This is mostly a work in progress, still keeping some code from a specific project
+
+#' @importFrom magrittr %>%
+#' @export
 prepare_kfold <- function(K, model_defs, base_data, seed = NULL) {
+    if (!requireNamespace("loo", quietly = TRUE)) {
+        stop("Package \"loo\" needed for this function to work. Please install it.",
+             call. = FALSE)
+    }
+
     model_names <- unique(model_defs$model_name)
-    
-    models = foreach(model_name = model_names) %do% {
-        library(rstan)  #On Windows, %dopar%-ed code does not share the main session
-        library(here)
-        rstan_options(auto_write = TRUE)
-        stan_model(here("stan", sprintf("%s.stan", model_name)))
-    } %>% setNames(model_names)
-    
-    model_defs <- model_defs %>% mutate(id = 1:n())
-    model_defs_kfold = model_defs %>% crossing(fold = 1:K)
-    
+
+    models <- model_names %>% purrr::map(function(model_name) {
+        rstan::rstan_options(auto_write = TRUE)
+        rstan::stan_model(here::here("stan", sprintf("%s.stan", model_name)))
+    }) %>% setNames(model_names)
+
+    model_defs <- model_defs %>% dplyr::mutate(id = 1:(dplyr::n()))
+    model_defs_kfold <- model_defs %>% dplyr::crossing(fold = 1:K)
+
     if (!is.null(seed)) {
         set.seed(seed)
     }
-    
-    folds = kfold_split_random(K, base_data$N)
-    data_list = list()
+
+    folds <- loo::kfold_split_random(K, base_data$N)
+    data_list <- list()
     for (i in 1:nrow(model_defs_kfold)) {
-        data_list[[i]] = base_data
-        data_list[[i]]$holdout = (folds == model_defs_kfold$fold[i])
+        data_list[[i]] <- base_data
+        data_list[[i]]$holdout <- (folds == model_defs_kfold$fold[i])
     }
-    
+
     if (!is.null(model_defs$adapt_delta)) {
-        control_list = map(model_defs_kfold$adapt_delta, function(x) {
+        control_list <- map(model_defs_kfold$adapt_delta, function(x) {
             list(adapt_delta = x)
         })
     } else {
-        control_list = NULL
+        control_list <- NULL
     }
-    
-    list(base_data = base_data, data_list = data_list, control_list = list, model_defs = model_defs, model_defs_kfold = model_defs_kfold, models_list = models[as.character(model_defs_kfold$model_name)], 
-        models_unique = models)
-    
+
+    list(base_data = base_data, data_list = data_list, control_list = list, model_defs = model_defs, model_defs_kfold = model_defs_kfold,
+        models_list = models[as.character(model_defs_kfold$model_name)], models_unique = models)
+
 }
 
+#' @importFrom magrittr %>%
+#' @export
 run_kfold <- function(kfold_def, name_for_cache) {
-    cache_dir = here("local_temp_data", name_for_cache)
+    if (!requireNamespace("matrixStats", quietly = TRUE)) {
+        stop("Package \"matrixStats\" needed for this function to work. Please install it.",
+             call. = FALSE)
+    }
+
+    cache_dir <- here::here("local_temp_data", name_for_cache)
     if (!dir.exists(cache_dir)) {
         dir.create(cache_dir)
     }
-    fits = sampling_multi(kfold_def$models_list, kfold_def$data_list, control_per_item = kfold_def$control_list, cache_dir = cache_dir, cores = getOption("mc.cores"))
-    
+    fits <- sampling_multi(kfold_def$models_list, kfold_def$data_list, control_per_item = kfold_def$control_list, cache_dir = cache_dir,
+        cores = getOption("mc.cores"))
+
     kfold_log_lik <- kfold_def$model_defs$id %>% map(function(id) {
-        indices = kfold_def$model_defs_kfold$id == id
+        indices <- kfold_def$model_defs_kfold$id == id
         fits_for_model <- fits[indices]
         holdout_for_model <- lapply(kfold_def$data_list[indices], "[[", "holdout")
         extract_log_lik_K(fits_for_model, holdout_for_model, "log_lik")
     })
-    
-    res_names <- kfold_def$model_defs %>% unite(full_name, -id) %>% pull(full_name)
-    kfold_res <- kfold_log_lik %>% map(kfold) %>% set_names(res_names)
-    
+
+    res_names <- kfold_def$model_defs %>% unite(full_name, -id) %>% dplyr::pull(full_name)
+    kfold_res <- kfold_log_lik %>% map(kfold) %>% rlang::set_names(res_names)
+
     list(fits = fits, log_lik = kfold_log_lik, kfold = kfold_res)
 }
 
+#' @importFrom magrittr %>%
 extract_holdout_ranks <- function(list_of_stanfits, list_of_holdout, base_data) {
     K <- length(list_of_stanfits)
     N_samples <- 100
@@ -69,63 +85,69 @@ extract_holdout_ranks <- function(list_of_stanfits, list_of_holdout, base_data) 
     if (any(is.na(counts_gen))) {
         stop("Inconsistent holdout data")
     }
-    
-    holdout_ranks_less <- sweep(all_genes_holdout_gen, MARGIN = c(2, 3), STATS = base_data$counts, FUN = "<") %>% apply(MARGIN = c(2, 3), FUN = sum)
-    
-    holdout_ranks_equal <- sweep(all_genes_holdout_gen, MARGIN = c(2, 3), STATS = base_data$counts, FUN = "==") %>% apply(MARGIN = c(2, 3), FUN = sum)
-    
-    # If there are equal values, sample the rank randomly over the equal values Using a trich with rounded uniform random numbers to get that in a
-    # vectorized way
-    holdout_ranks = holdout_ranks_less + round((holdout_ranks_equal + 1) * array(runif(length(holdout_ranks_equal)), dim(holdout_ranks_equal)) - 0.5)
-    
+
+    holdout_ranks_less <- sweep(all_genes_holdout_gen, MARGIN = c(2, 3), STATS = base_data$counts, FUN = "<") %>% apply(MARGIN = c(2,
+        3), FUN = sum)
+
+    holdout_ranks_equal <- sweep(all_genes_holdout_gen, MARGIN = c(2, 3), STATS = base_data$counts, FUN = "==") %>% apply(MARGIN = c(2,
+        3), FUN = sum)
+
+    # If there are equal values, sample the rank randomly over the equal values Using a trich with rounded uniform random
+    # numbers to get that in a vectorized way
+    holdout_ranks <- holdout_ranks_less + round((holdout_ranks_equal + 1) * array(runif(length(holdout_ranks_equal)), dim(holdout_ranks_equal)) -
+        0.5)
+
     dimnames(holdout_ranks) <- dimnames(base_data$counts)
     holdout_ranks
 }
 
+#' @importFrom magrittr %>%
 extract_holdout_ranks_all <- function(kfold_def, kfold_res) {
     kfold_def$model_defs$id %>% map(function(id) {
-        indices = kfold_def$model_defs_kfold$id == id
+        indices <- kfold_def$model_defs_kfold$id == id
         fits_for_model <- kfold_res$fits[indices]
         holdout_for_model <- lapply(kfold_def$data_list[indices], "[[", "holdout")
-        extract_holdout_ranks(fits_for_model, holdout_for_model, kfold_def$base_data) %>% as.tibble() %>% rownames_to_column("sample") %>% gather("gene", 
-            "rank", -sample) %>% mutate(model = kfold_def$model_defs$model_name[kfold_def$model_defs$id == id])
+        extract_holdout_ranks(fits_for_model, holdout_for_model, kfold_def$base_data) %>% as.tibble() %>% rownames_to_column("sample") %>%
+            gather("gene", "rank", -sample) %>% mutate(model = kfold_def$model_defs$model_name[kfold_def$model_defs$id == id])
     }) %>% do.call(rbind, args = .)
 }
 
+#' @importFrom magrittr %>%
 plot_holdout_ranks <- function(ranks, binwidth = 1, facet = ~model) {
-    if (100%%binwidth != 0) {
+    if (100 %% binwidth != 0) {
         stop("binwidth has to divide 100")
     }
-    
+
     n_ranks <- aggregate(update.formula(facet, rank ~ .), ranks, length)
     if (length(unique(n_ranks$rank)) != 1) {
         stop("Unequal number of observations per group")
     }
-    
+
     n_ranks <- n_ranks$rank[1]
-    
-    CI = qbinom(c(0.005, 0.5, 0.995), size = n_ranks, prob = binwidth/100)
-    lower = CI[1]
-    mean = CI[2]
-    upper = CI[3]
-    
-    
-    ranks %>% ggplot(aes(x = rank)) + geom_segment(aes(x = 0, y = mean, xend = 100, yend = mean), colour = "grey25") + geom_polygon(data = data.frame(x = c(-10, 
-        0, -10, 110, 100, 110, -10), y = c(lower, mean, upper, upper, mean, lower, lower)), aes(x = x, y = y), fill = "grey45", color = "grey25", alpha = 0.5) + 
-        geom_histogram(breaks = seq(1, 101, by = binwidth), closed = "left", fill = "#A25050", colour = "black") + facet_wrap(facet, scales = "free_y") + 
-        ggtitle("Posterior ranks of heldout observations")
-    
+
+    CI <- qbinom(c(0.005, 0.5, 0.995), size = n_ranks, prob = binwidth / 100)
+    lower <- CI[1]
+    mean <- CI[2]
+    upper <- CI[3]
+
+
+    ranks %>% ggplot(aes(x = rank)) + geom_segment(aes(x = 0, y = mean, xend = 100, yend = mean), colour = "grey25") + geom_polygon(data = data.frame(x = c(-10,
+        0, -10, 110, 100, 110, -10), y = c(lower, mean, upper, upper, mean, lower, lower)), aes(x = x, y = y), fill = "grey45",
+        color = "grey25", alpha = 0.5) + geom_histogram(breaks = seq(1, 101, by = binwidth), closed = "left", fill = "#A25050",
+        colour = "black") + facet_wrap(facet, scales = "free_y") + ggtitle("Posterior ranks of heldout observations")
+
 }
 
-# Functions extract_log_lik and kfold taken from https://github.com/stan-dev/stancon_talks/blob/master/2017/Contributed-Talks/07_nicenboim/kfold.Rmd
+# Functions extract_log_lik and kfold taken from
+# https://github.com/stan-dev/stancon_talks/blob/master/2017/Contributed-Talks/07_nicenboim/kfold.Rmd
 
 extract_log_lik_K <- function(list_of_stanfits, list_of_holdout, ...) {
     K <- length(list_of_stanfits)
     list_of_log_liks <- plyr::llply(1:K, function(k) {
         extract_log_lik(list_of_stanfits[[k]], ...)
     })
-    # `log_lik_heldout` will include the loglike of all the held out data of all the folds.  We define `log_lik_heldout` as a (samples x N_obs) matrix
-    # (similar to each log_lik matrix)
+    # `log_lik_heldout` will include the loglike of all the held out data of all the folds.  We define `log_lik_heldout` as a
+    # (samples x N_obs) matrix (similar to each log_lik matrix)
     log_lik_heldout <- list_of_log_liks[[1]] * NA
     for (k in 1:K) {
         log_lik <- list_of_log_liks[[k]]
@@ -144,26 +166,28 @@ extract_log_lik_K <- function(list_of_stanfits, list_of_holdout, ...) {
 }
 
 kfold <- function(log_lik_heldout) {
-    library(matrixStats)
-    logColMeansExp <- function(x) {
+    if (!requireNamespace("matrixStats", quietly = TRUE)) {
+        stop("Package \"matrixStats\" needed for this function to work. Please install it.",
+             call. = FALSE)
+    }
+    log_col_means_exp <- function(x) {
         # should be more stable than log(colMeans(exp(x)))
         S <- nrow(x)
-        colLogSumExps(x) - log(S)
+        matrixStats::colLogSumExps(x) - log(S)
     }
     # See equation (20) of @VehtariEtAl2016
-    pointwise <- matrix(logColMeansExp(log_lik_heldout), ncol = 1)
+    pointwise <- matrix(log_col_means_exp(log_lik_heldout), ncol = 1)
     colnames(pointwise) <- "elpd"
     # See equation (21) of @VehtariEtAl2016
     elpd_kfold <- sum(pointwise)
     se_elpd_kfold <- sqrt(ncol(log_lik_heldout) * var(pointwise))
-    
+
     estimates <- matrix(NA_real_, nrow = 1, ncol = 2)
     rownames(estimates) <- "elpd_loo"
     colnames(estimates) <- c("Estimate", "SE")
     estimates["elpd_loo", "Estimate"] <- elpd_kfold
     estimates["elpd_loo", "SE"] <- se_elpd_kfold
-    
+
     out <- list(pointwise = pointwise, estimates = estimates)
     structure(out, class = "loo")
 }
-
--- R/sampling_multi.R
+++ R/sampling_multi.R
@@ -23,62 +23,63 @@
 #' @return A list of length `length(data)` containing the result of applying
 #'   `combine_fun` to each set of `chains` result of applying `map_fun`
 #'   to each fit.
-sampling_multi <- function(models, data, map_fun = sampling_multi_noop, combine_fun = sflist2stanfit, chains = 4, cores = parallel::detectCores(), init = NULL, 
-    control = NULL, init_per_item = NULL, control_per_item = NULL, map_fun_dependencies = c(), R_session_init_expr = NULL, cache_dir = NULL, ids_to_compute = 1:length(data), 
-    ...) {
-    
+#' @export
+sampling_multi <- function(models, data, map_fun = sampling_multi_noop, combine_fun = sflist2stanfit, chains = 4, cores = parallel::detectCores(),
+    init = NULL, control = NULL, init_per_item = NULL, control_per_item = NULL, map_fun_dependencies = c(), R_session_init_expr = NULL,
+    cache_dir = NULL, ids_to_compute = 1:length(data), ...) {
+
     # TODO: argument validation
     if (!is.null(cache_dir) && !dir.exists(cache_dir)) {
         stop(paste0("Cache dir '", cache_dir, "'  does not exist"))
     }
-    
+
     cl <- parallel::makeCluster(cores, useXDR = FALSE)
     on.exit(parallel::stopCluster(cl))
-    
+
     # Prepare argument lists for all calls
     .dotlists_per_item <- list()
     for (data_id in 1:length(data)) {
         if (is.list(models)) {
-            model = models[[data_id]]
+            model <- models[[data_id]]
         } else {
-            model = models
+            model <- models
         }
         if (class(model) != "stanmodel") {
             stop(paste0("Model for data_id ", data_id, " is not of class 'stanmodel'"))
         }
-        
+
         for (chain_id in 1:chains) {
-            
+
             .dotlist <- c(list(model, chains = 1L, cores = 0L), list(...))
-            
+
             if (is.list(init_per_item)) {
                 .dotlist$init <- init_per_item[[data_id]]
                 # TODO check that init is not set
             } else {
                 .dotlist$init <- init
             }
-            
+
             # Handle per-chain inits
-            if (is.list(.dotlist$init)) 
+            if (is.list(.dotlist$init))
                 .dotlist$init <- .dotlist$init[chain_id]
-            
+
             if (is.list(control_per_item)) {
                 .dotlist$control <- control_per_item[[data_id]]
                 # TODO check that control is not set
             } else {
                 .dotlist$control <- control
             }
-            
+
             .dotlist$chain_id <- chain_id
-            
+
             .dotlist$data <- data[[data_id]]
-            
-            
+
+
             .dotlists_per_item[[(data_id - 1) * chains + chain_id]] <- .dotlist
         }
     }
-    
-    
+
+
     fit_fun <- function(i) {
         cached <- FALSE
         not_cached_msg <- ""
@@ -87,19 +88,19 @@ sampling_multi <- function(models, data, map_fun = sampling_multi_noop, combine_
             filename <- sprintf("%s/%06d.rds", cache_dir, i)
             if (file.exists(filename)) {
                 cached_data <- readRDS(filename)
-                
+
                 if (!is.list(cached_data)) {
                   not_cached_msg <- "Unexpected data format"
                   cached <- FALSE
                 } else {
                   fit_from_cache <- cached_data$fit
-                  
+
                   model <- arg_list[[1]]
-                  
+
                   if (class(model) != "stanmodel") {
                     stop("Argument 1 is not a stan model")
                   }
-                  
+
                   if (is.null(cached_data$data) || is.null(fit_from_cache)) {
                     not_cached_msg <- "Does not contain fit or data"
                     cached <- FALSE
@@ -119,20 +120,20 @@ sampling_multi <- function(models, data, map_fun = sampling_multi_noop, combine_
                 }
             }
         }
-        
+
         if (!cached) {
             single_fit <- do.call(rstan::sampling, args = arg_list)
             if (!is.null(cache_dir)) {
                 saveRDS(list(data = arg_list$data, fit = single_fit), filename)
             }
         }
-        
+
         # TODO should catch error from map_fun
         list(cached = cached, not_cached_msg = not_cached_msg, result = map_fun(single_fit, data_id, chain_id))
     }
-    
-    
-    
+
+
+
     dependencies <- c("rstan", "Rcpp", map_fun_dependencies)
     .paths <- unique(c(.libPaths(), sapply(dependencies, FUN = function(d) {
         dirname(system.file(package = d))
@@ -143,21 +144,21 @@ sampling_multi <- function(models, data, map_fun = sampling_multi_noop, combine_
     parallel::clusterEvalQ(cl, expr = for (dep in dependencies) {
         suppressPackageStartupMessages(require(dep, quietly = TRUE, character.only = TRUE))
     })
-    
+
     parallel::clusterExport(cl, varlist = ".dotlists_per_item", envir = environment())
-    
+
     parallel::clusterExport(cl, varlist = c("map_fun", "R_session_init_expr"), envir = environment())
-    
+
     parallel::clusterEvalQ(cl, expr = R_session_init_expr)
-    
+
     ids <- rep(ids_to_compute, each = chains)
-    items <- ((rep(ids_to_compute, each = chains) - 1) * chains + rep(1:chains, times = length(ids_to_compute)))
-    
+    items <- (rep(ids_to_compute, each = chains) - 1) * chains + rep(1:chains, times = length(ids_to_compute))
+
     results_raw <- parallel::parLapplyLB(cl, X = items, fun = fit_fun, chunk.size = 1)
     results_flat <- purrr::map(results_raw, function(x) {
         x$result
     })
-    
+
     results <- list()
     if (!is.null(cache_dir)) {
         cache_info <- purrr::map_dfr(results_raw, function(x) {
@@ -177,17 +178,19 @@ sampling_multi <- function(models, data, map_fun = sampling_multi_noop, combine_
     results
 }
 
+#' @export
 sampling_multi_noop <- function(fit, data_id, chain_id) {
     fit
 }
 
 #' @return Returns a function to be used as `map_fun` in [sampling_multi()]
 #'   that stores all fits in RDS files
+#' @export
 sampling_multi_store_file_generator <- function(base_dir, base_name) {
     force(base_dir)
     force(base_name)
     function(fit, data_id, chain_id) {
-        filename = paste0(base_dir, "/", base_name, "_", data_id, "_", chain_id, ".rds")
+        filename <- paste0(base_dir, "/", base_name, "_", data_id, "_", chain_id, ".rds")
         saveRDS(fit, filename)
         filename
     }
